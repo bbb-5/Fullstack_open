b608d6a061c5090f4f3d5f3d950abc8e431b0563
bbb-5 <aliisa.torkki@kolumbus.fi>
2026-06-24 12:52:04 +0300

    2.4 more courses

--- osa2/kurssitiedot/src/App.jsx
+++ osa2/kurssitiedot/src/App.jsx
@@ -2,7 +2,7 @@ const Header = ({course_name}) => {
 
   return (
     <>
-      <h1>{course_name}</h1>
+      <h2>{course_name}</h2>
     </>
   )
 }
@@ -34,7 +34,7 @@ const Total = ({parts}) => {
   return (
     <>
       <p><b>Total of {parts.reduce((s,p) =>
-      s + p.exercises,0)}</b></p>
+      s + p.exercises,0)} exercises</b></p>
     </>
   )
 
@@ -53,36 +53,57 @@ const Course = ({course}) => {
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
 
   return (
     <div>
-      <Course course={course}/>
+      <h1>Web development curriculum</h1>
+      {courses.map(course => 
+        <Course course={course} key={course.id}/>)}
     </div>
   )
 }
