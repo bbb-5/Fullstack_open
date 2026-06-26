4413eb7c8307ba90b46a7cc3511916b266167c0d
bbb-5 <aliisa.torkki@kolumbus.fi>
2026-06-26 14:11:13 +0300

    2.6 add names

---
+++ osa2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+
+const Person = ({person_name }) => {
+  return (
+    <p key={person_name}>{person_name}</p>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    const new_person = {
+      name: newName
+    }
+
+    setPersons(persons.concat(new_person))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => 
+        <Person key={person.name} person_name={person.name}/>
+      )}
+    </div>
+  )
+
+}
+
+export default App
