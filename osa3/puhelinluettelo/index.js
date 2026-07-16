20fa82fb9af863a7304215cb4264a67ce86c34e7
bbb-5 <aliisa.torkki@kolumbus.fi>
2026-07-16 14:22:34 +0300

    3.11 front to back

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -7,6 +7,7 @@ app.use(cors())
 app.use(express.json())
 //app.use(morgan('tiny'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('dist'))
 
 let persons = [
     { 
