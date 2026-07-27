0041d65f81fd10f1c2fc9ad044553d392ddb576d
bbb-5 <aliisa.torkki@kolumbus.fi>
2026-07-27 09:39:23 +0300

    3.13 & 3.14

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -1,41 +1,20 @@
-const http = require('http')
+require('dotenv').config()
+const Person = require('./models/person')
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
 app.use(express.json())
-//app.use(morgan('tiny'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(express.static('dist'))
-
-let persons = [
-    { 
-      "name": "Arto Hellas", 
-      "number": "040-123456",
-      "id": "1"
-    },
-    { 
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523",
-      "id": "2"
-    },
-    { 
-      "name": "Dan Abramov", 
-      "number": "12-43-234345",
-      "id": "3"
-    },
-    { 
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122",
-      "id": "4"
-    }
-  ]
 
 app.get('/', (request, response) => {
   response.send('<h1>Tervetuloa puhelinluetteloon!</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/info', (request, response) => {
@@ -46,14 +25,14 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(p => p.id === id)
-  
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+  })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -73,39 +52,22 @@ const generateId = () => {
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
-  if (!body.name) {
-    return response.status(400).json({ 
-      error: 'Name Missing' 
-    })
-  }
+  if (!body.name) return response.status(400).json({ error: 'Name Missing' })
+  if (!body.number) return response.status(400).json({ error: 'Number Missing' })
 
-  if (!body.number) {
-    return response.status(400).json({ 
-      error: 'Number Missing' 
-    })
-  }
-
-  if (persons.find(p => p.name.toLowerCase() === body.name.toLowerCase())) {
-    return response.status(400).json({ 
-      error: 'Name must be unique' 
-    })
-  }
-
-  const  person = {
+  const  person = new Person({
     name: body.name,
-    number: body.number || '',
-    id: generateId()
-  }
+    number: body.number,
+  })
 
-  
-  persons = persons.concat(person)
-  
-  response.json(person)
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
 
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
 
--- osa3/puhelinluettelo/mongo.js
+++ osa3/puhelinluettelo/mongo.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
@@ -11,11 +12,10 @@ if (process.argv.length < 3) {
   process.exit(1)
 }
 
-const password = process.argv[2]
 const name = process.argv[3]
 const number = process.argv[4]
 
-const url = `mongodb+srv://mamelukkikala:${password}@cluster0.haroy84.mongodb.net/PhoneBook?retryWrites=true&w=majority&appName=Cluster0`
+const url = process.env.MONGODB_URI
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url, { family: 4 })
