e1b179c389dbe5e162cfbcf0169f10b70fa3d0db
bbb-5 <aliisa.torkki@kolumbus.fi>
2026-07-27 10:55:51 +0300

    3.15 & 3.16

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -3,9 +3,23 @@ const Person = require('./models/person')
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-app.use(express.static('dist'))
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 app.get('/', (request, response) => {
   response.send('<h1>Tervetuloa puhelinluetteloon!</h1>')
@@ -24,7 +38,7 @@ app.get('/info', (request, response) => {
     <p>${date}</p>`)
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then(person => {
       if (person) {
@@ -33,21 +47,16 @@ app.get('/api/persons/:id', (request, response) => {
         response.status(404).end()
       }
   })
+  .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(p => p.id !== id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
-
-const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => Number(n.id)))
-    : 0
-  return String(maxId + 1)
-}
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
@@ -66,6 +75,9 @@ app.post('/api/persons', (request, response) => {
 })
 
 morgan.token('body', (req, res) => JSON.stringify(req.body))
+
+app.use(errorHandler)
+app.use(unknownEndpoint)
 
 const PORT = process.env.PORT
 app.listen(PORT)
