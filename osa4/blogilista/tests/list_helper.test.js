30855f2c4d87322e72839efa10440cfb26f746d5
bbb-5 <aliisa.torkki@kolumbus.fi>
2026-08-12 15:03:09 +0300

    4.6 mostBlogs

--- osa4/blogilista/tests/list_helper.test.js
+++ osa4/blogilista/tests/list_helper.test.js
@@ -50,14 +50,6 @@ const blogs = [
     url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
     likes: 2,
     __v: 0
-  },
-  {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 0,
-    __v: 0
   }
 ]
 
@@ -101,5 +93,33 @@ describe('favorite blog', () => {
     const max = Math.max.apply(null,
       blogs.map(function (o) { return o.likes }))
     assert.strictEqual(result, max)
+  })
+})
+
+describe('most blogs', () => {
+
+  test('returns null with empty', () => {
+    const result = listHelper.mostBlogs([])
+    assert.deepStrictEqual(result, null)
+  })
+
+  test('returns most blogs from many blogs', () => {
+    const result = listHelper.mostBlogs(blogs)
+    assert.deepStrictEqual(result, { author: 'Robert C. Martin', blogs: 3 })
+  })
+
+  test('returns 1 with 1', () => {
+    const result = listHelper.mostBlogs([blogs[3]])
+    assert.deepStrictEqual(result, { author: 'Robert C. Martin', blogs: 1 })
+  })
+
+  test('returns someone when 2 same most blogcounts', () => {
+    const result = listHelper.mostBlogs([blogs[0],blogs[1]])
+    assert.strictEqual(result.blogs, 1)
+  })
+
+  test('returns someone when same most blogcounts', () => {
+    const result = listHelper.mostBlogs([blogs[1],blogs[1],blogs[3],blogs[4]])
+    assert.strictEqual(result.blogs, 2)
   })
 })
--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -11,14 +11,34 @@ const totalLikes = (blogs) => {
 const favoriteBlog = (blogs) => {
   if (blogs.length === 0) return null
 
-  let fav = blogs.reduce(function(prev, current) {
-    return (prev.likes > current.likes) ? prev : current
-  })
-  return fav
+  return blogs.reduce((prev, current) =>
+    (prev.likes > current.likes) ? prev : current)
+}
+
+const mostBlogs = (blogs) => {
+
+  if (blogs.length === 0) return null
+
+  let blogCounts = {}
+  let maxBlogsAuthor = ''
+  let maxBlogs = 0
+
+  for (let i = 0; i < blogs.length; i++) {
+    const author = blogs[i].author
+    blogCounts[author] = (blogCounts[author] || 0 ) + 1
+
+    if (blogCounts[author] > maxBlogs) {
+      maxBlogsAuthor = author
+      maxBlogs = blogCounts[author]
+    }
+  }
+
+  return { author: maxBlogsAuthor, blogs: maxBlogs }
 }
 
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
